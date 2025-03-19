17e65af963c9d7cc50906d787c07fb473f1795d1
Jeffrey Bernadas <jeffrey.bernadas0@gmail.com>
2025-03-19 21:58:22 +0800

    Init: Initialize App Shell Microfrontend

---
+++ app-shell/module-federation.config.ts
@@ -0,0 +1,8 @@
+export const mfConfig = {
+  name: "app_shell",
+  remotes: {
+    "@core": "core@https://core.thecodebit.digital/remoteEntry.js",
+  },
+  exposes: {},
+  shared: ["react", "react-dom"],
+};
